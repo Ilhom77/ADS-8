8bdb32840e4e3bd26dfcc853d4ef1d72e25e5efc
Ilkhom Bokhirov <103019100+Ilhom77@users.noreply.github.com>
2022-06-01 01:23:16 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,60 +1,59 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
-
-Train::Cage* Train::create(bool light) {
-    Cage* cage = new Cage;
-    cage->light = light;
-    cage->next = nullptr;
-    cage->prev = nullptr;
-    return cage;
+Train::Cage* Train::create(bool _light) {
+  Cage *cage = new Cage;
+  cage->light = _light;
+  cage->next = nullptr;
+  cage->prev = nullptr;
+  return cage;
 }
-void Train::addCage(bool light) {
-    if (last && first) {
-        last->next = create(light);
-        last->next->prev = last;
-        last = last->next;
-        first->prev = last;
-        last->next = first;
-    } else {
-        first = create(light);
-        last = first;
-    }
+void Train::addCage(bool _light) {
+  if (last && first) {
+    last->next = nullptr;
+    last->next = create(_light);
+    last->next->prev = last;
+    last = last->next;
+    last->next = first;
+    first->prev = last;
+  } else {
+      first = create(_light);
+      last = first;
+  }
 }
-
 int Train::getLength() {
-    int len = 0;
-    first->light = true;
-    Cage* temp = first;
-    while (true) {
-        len++;
-        for (int i = 0; i < len; ++i) {
-            temp = temp->next;
-            countOp++;
-        }
-        if (temp->light) {
-            temp->light = false;
-            for (int i = 0; i < len; ++i) {
-                temp = temp->prev;
-                countOp++;
-            }
-        } else {
-            while (!temp->light) {
-                len++;
-                temp = temp->next;
-                countOp++;
-            }
-            temp->light = false;
-            for (int i = 0; i < len; ++i) {
-                temp = temp->prev;
-                countOp++;
-            }
-        }
-        if (!temp->light) {
-            break;
-        }
+  Cage *temp = first;
+  bool current;
+  int steps_count = 0;
+  if (!(temp->light)) {
+    temp->light = !(temp->light);
+  }
+  current = (temp->light);
+  while (temp) {
+    temp = temp -> next;
+    ++steps_count;
+    ++countOp;
+    if (temp->light == current) {
+      temp->light = !current;
+      break;
     }
-    return len;
+  }
+  for (int i = 0; i < steps_count; ++i) {
+    temp = temp->prev;
+    ++countOp;
+  }
+  if (temp->light != current) {
+    return steps_count;
+  }
+  return getLength();
 }
-
 int Train::getOpCount() {
-    return countOp;
+  return countOp;
+}
+Train::~Train() {
+  int length = (*this).getLength();
+  for (int i = 0; i < length; ++i) {
+    Cage *temp = first;
+    first = first->next;
+    delete temp;
+  }
+}
